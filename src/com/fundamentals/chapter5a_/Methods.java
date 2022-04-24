fba5e7d47d8719af4c5901da10179e4204ed1498
German147 <barreragerman27@gmail.com>
2022-04-24 11:10:20 -0300

    first commit

---
+++ src/com/fundamentals/chapter5a_/Methods.java
@@ -0,0 +1,14 @@
+package com.fundamentals.chapter5a_;
+
+public class Methods {
+    public static void main(String[] args) {
+
+        int uno = 1;
+        int dos = 2;
+        public static int calculateSum ( int number1, int number2){
+            int sum = uno + dos;
+            return sum;
+        }
+
+    }
+}
