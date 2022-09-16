8c60534fc5916ac51a086fe6f52f41a9f296a45c
German <barreragerman27@gmail.com>
2022-09-16 19:12:49 -0300

    Practice on reverse Strings

---
+++ src/algoritmiaCodes/algo/StringReverse.java
@@ -0,0 +1,34 @@
+package algoritmiaCodes.algo;
+
+import java.util.Locale;
+
+public class StringReverse {
+
+      //  private static final Logger LOGGER = Logger.getLogger(Runner.class.getName());
+        public static void main(String[] args) {
+            System.out.println("Reversed string: " + reverse("German"));
+        }
+
+    /**
+     * Here I did Debug and learnt how the code works
+     * @param input
+     * @return
+     */
+    public static String reverse(String input){
+           // input = input.toLowerCase(Locale.ROOT).replaceAll("[^a-z0-9]", "");
+            char[] in = input.toCharArray();
+            int begin=0;
+            int end=in.length-1;
+            char temp;
+
+            while(end>begin){
+                temp = in[begin];
+                in[begin]=in[end];
+                in[end] = temp;
+                end--;
+                begin++;
+            }
+            return new String(in);
+        }
+
+}
