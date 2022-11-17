b1b4c7bc981450caa3880c1027bae30fa9956e41
German <barreragerman27@gmail.com>
2022-11-17 17:48:59 -0300

    WhileLoops was created for practice and understanding

---
+++ src/algoritmiaCodes/algo/loops/WhileLoops.java
@@ -0,0 +1,18 @@
+package algoritmiaCodes.algo.loops;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int a[] = {10, 20, 30, 40, 50};
+        int size = a.length;
+        int sum = 0;
+        int i = 0;
+
+        while (i!=size){
+            sum = sum + a[i];
+            ++i;
+        }
+        System.out.println(sum);
+
+    }
+}
