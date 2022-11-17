93cb3c38951f2185a3fa2009eb861796c8e2957a
German <barreragerman27@gmail.com>
2022-11-17 19:37:00 -0300

    It was added some comments to the class ConvertAStringToMap  to clarify the code flow...

--- src/algoritmiaCodes/algo/ConvertAStringToMap.java
+++ src/algoritmiaCodes/algo/ConvertAStringToMap.java
@@ -10,22 +10,29 @@ public class ConvertAStringToMap {
         String x = "lliiopp";
         char y[] = x.toCharArray();
         int size = y.length;
-
+        // here I create a map using Character and Integer as a Key Value pair
         Map<Character, Integer> map = new HashMap<>();
         int i = 0;
         while (i != size) {
+            //Here I say if map contains a certain key and if it s false...
             if (map.containsKey(y[i]) == false) {
+                // put that key a value of 1
                 map.put(y[i], 1);
             } else {
+                //add to the oldValue variable that key with its value
                 int oldValue = map.get(y[i]);
+                //Here I sum a new value to the Key
                 int newValue = oldValue + 1;
+                // and put its new value
                 map.put(y[i], newValue);
             }
             ++i;
         }
 
+
+        //in order to print the key value pair I use this forEach loop
         Set<Map.Entry<Character, Integer>> hmap = map.entrySet();
-        for (Map.Entry<Character,Integer> data : hmap) {
+        for (Map.Entry<Character, Integer> data : hmap) {
             System.out.print(data.getKey());
             System.out.println(data.getValue());
         }
---
+++ src/algoritmiaCodes/algo/loops/ForEachLoop.java
@@ -0,0 +1,16 @@
+package algoritmiaCodes.algo.loops;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+
+        int a[] = {10, 20, 30, 40, 50};
+       // int size = a.length;
+        int sum = 0;
+
+        for (int data : a) {
+            sum = sum + data;
+        }
+        System.out.println(sum);
+
+    }
+}
