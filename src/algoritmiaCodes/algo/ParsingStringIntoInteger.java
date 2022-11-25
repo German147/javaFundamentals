77ed6dad6071ba3fb5c7548f23a92e1930c42d67
German <barreragerman27@gmail.com>
2022-11-25 13:38:42 -0300

    Parsing s String into Integer

---
+++ src/algoritmiaCodes/algo/ParsingStringIntoInteger.java
@@ -0,0 +1,10 @@
+package algoritmiaCodes.algo;
+
+public class ParsingStringIntoInteger {
+    public static void main(String[] args) {
+        String x = "27";
+        int y = Integer.parseInt(x);
+
+        System.out.println(y);
+    }
+}
